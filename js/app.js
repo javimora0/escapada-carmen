f6fe37165e07bf1c13d414b2e1b9ee4a76d7991a
Javier Morales <jmoralesr@protonmail.com>
2026-09-02 21:27:31 +0200

    add primera pista

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 const startButton = document.getElementById("startButton");
 const gameScreen = document.getElementById("gameScreen");
+const clueScreen = document.getElementById("clueScreen");
+
+const answerButtons = document.querySelectorAll(".answer");
+const continueButton = document.getElementById("continueButton");
+
+
+// ======================================
+// COMENZAR LA AVENTURA
+// ======================================
 
 startButton.addEventListener("click", () => {
 
@@ -8,6 +17,48 @@ startButton.addEventListener("click", () => {
     setTimeout(() => {
 
         document.querySelector(".welcome").style.visibility = "hidden";
+
+        gameScreen.classList.add("active");
+
+    }, 700);
+
+});
+
+
+// ======================================
+// RESPUESTA A LA PREGUNTA 1
+// ======================================
+
+answerButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const answer = button.dataset.answer;
+
+        console.log("Respuesta elegida:", answer);
+
+        gameScreen.classList.remove("active");
+
+        setTimeout(() => {
+
+            clueScreen.classList.add("active");
+
+        }, 700);
+
+    });
+
+});
+
+
+// ======================================
+// CONTINUAR
+// ======================================
+
+continueButton.addEventListener("click", () => {
+
+    clueScreen.classList.remove("active");
+
+    setTimeout(() => {
 
         gameScreen.classList.add("active");
 
