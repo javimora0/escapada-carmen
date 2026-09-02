1615d9c8ff9645fed86cc03726c4cfc2e80c6e09
Javier Morales <jmoralesr@protonmail.com>
2026-09-02 21:22:22 +0200

    add first answer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("La sorpresa acaba de empezar ❤️");
+const startButton = document.getElementById("startButton");
+const gameScreen = document.getElementById("gameScreen");
+
+startButton.addEventListener("click", () => {
+
+    document.querySelector(".welcome").style.opacity = "0";
+
+    setTimeout(() => {
+
+        document.querySelector(".welcome").style.visibility = "hidden";
+
+        gameScreen.classList.add("active");
+
+    }, 700);
+
+});
